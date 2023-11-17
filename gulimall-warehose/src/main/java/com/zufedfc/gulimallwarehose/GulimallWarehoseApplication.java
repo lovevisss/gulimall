415d43e7c954154d028abae7f72ac96ae4d3b6a6
zsy <530822987@qq.com>
2023-11-17 11:17:54 +0800

    init2

---
+++ gulimall-warehose/src/main/java/com/zufedfc/gulimallwarehose/GulimallWarehoseApplication.java
@@ -0,0 +1,13 @@
+package com.zufedfc.gulimallwarehose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallWarehoseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallWarehoseApplication.class, args);
+    }
+
+}
---
+++ gulimall-warehose/src/test/java/com/zufedfc/gulimallwarehose/GulimallWarehoseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zufedfc.gulimallwarehose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWarehoseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
